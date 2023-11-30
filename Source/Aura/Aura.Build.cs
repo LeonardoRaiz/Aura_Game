6029e58783e2fbea9ca900e85f89fa7924d3ef18
LeonardoRaiz <leonardoraiz@icloud.com>
2023-11-30 14:48:26 -0300

    Aura player class

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -8,8 +8,8 @@ public class Aura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		// Ter acesso ao modulo EnhancedInput
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
